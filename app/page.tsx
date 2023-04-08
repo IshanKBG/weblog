eb621b2364d16c748970a5469fc53d0574ac5fd0
IshanKBG <jaiswalishan701@gmail.com>
2023-04-08 18:22:22 +0530

    Better end to end typesafety

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 import { blog } from "@/lib/blog-sdk"
 import Link from "next/link"
 export default async function Home() {
-  const posts = await blog.listAllPosts();
+  const posts = await blog.getAllPosts();
   console.log(posts)
   return (
     <>
-      {posts.map((post, index) => (
-        <section key={index}>
-          <div>
-            <h2>
-              <Link href={post.slug}>
-                {post.title}
-              </Link>
-            </h2>
-          </div>
-        </section>
-      ))}
+      {posts.map((post) => {
+        <h1>{post.title}</h1>
+      })}
     </>
   )
 
--- lib/blog-sdk.ts
+++ lib/blog-sdk.ts
@@ -1,173 +1,46 @@
-import { z } from "zod";
+import { client } from "@/lib/notion";
 import { getEnv } from "@/lib/env";
-const blogMetaSchema = z.object({
-  title: z.string(),
-  postedAt: z.date(),
-  tags: z.string().array(),
-  slug: z.string(),
-})
-type BlogSchema = z.infer<typeof blogMetaSchema>
+import { Client } from "@notionhq/client";
+import { PageObjectResponse, PartialPageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { PostMeta } from "@/models/posts";
+interface ClientOpts {
+  client: Client
+  database_id: string
+}
+type ExtendNotionType = PageObjectResponse | PartialPageObjectResponse & {
+  properties: Object
+}
 export class BlogClient {
-  #token: string;
+  #client: Client;
   #database_id: string;
-  constructor(private token: string, private database_id: string) {
-    this.#token = token
-    this.#database_id = database_id
+  constructor(opts: ClientOpts) {
+    this.#client = opts.client;
+    this.#database_id = opts.database_id
   }
-  async listAllPosts(): Promise<Array<BlogSchema>> {
-    let res = await fetch(`https://api.notion.com/v1/databases/${this.#database_id}/query`, {
-      method: "POST",
-      headers: {
-        'Authorization': `Bearer ${this.#token}`,
-        'Notion-Version': '2022-06-28',
-        'Content-Type': 'application/json',
 
-      },
-      body: JSON.stringify({
-        filter: {
-          and: [
-            { property: 'published', checkbox: { equals: true } },
-
-          ],
-        },
-      }),
-      next: {
-        revalidate: 9
+  async getAllPosts() {
+    let response = await this.#client.databases.query({
+      database_id: this.#database_id,
+      filter: {
+        property: "draft",
+        checkbox: {
+          equals: true
+        }
       }
     })
-    const posts = await res.json();
-    const allPosts: NotionPageArray[] = posts.results
-    console.log(JSON.stringify(posts.results))
-    return allPosts.map((post: any) => {
-      return blogMetaSchema.parse(this.getPageMetaData(post));
-    });
-  }
-  getPageMetaData(post: NotionPageArray): BlogSchema {
-    const getTags = (tags: any) => {
-      const allTags = tags.map((tag: any) => {
-        return tag.name;
-      });
-
-      return allTags;
-    };
-
-    return {
-      title: post.properties.title.title[0].plain_text,
-      tags: getTags(post.properties.tags.multi_select),
-      postedAt: new Date(post.properties.postedAt.date.start),
-      slug: post.properties.slug.rich_text[0].plain_text,
-    }
+    let results = response.results
+    return results.map((res: any) => {
+      return PostMeta.parse({
+        title: res.properties.title.title[0].plain_text,
+        draft: res.properties.draft.checkbox,
+        desc: res.properties?.desc?.rich_text[0].plain_text,
+        postedAt: new Date(res.properties.postedAt.date.start),
+        slug: res.properties.slug.rich_text[0].plain_text,
+      })
+    })
   }
 }
-export const blog = new BlogClient(getEnv().NOTION_TOKEN, getEnv().NOTION_DATABASE_ID);
-type NotionPageArray = {
-  object: string;
-  id: string;
-  created_time: string;
-  last_edited_time: string;
-  created_by: {
-    object: string;
-    id: string;
-  };
-  last_edited_by: {
-    object: string;
-    id: string;
-  };
-  cover: null;
-  icon: null;
-  parent: {
-    type: string;
-    database_id: string;
-  };
-  archived: boolean;
-  properties: {
-    slug: {
-      id: string;
-      type: string;
-      rich_text: {
-        type: string;
-        text: {
-          content: string;
-          link: any;
-        };
-        annotations: {
-          bold: boolean;
-          italic: boolean;
-          strikethrough: boolean;
-          underline: boolean;
-          code: boolean;
-          color: string;
-        };
-        plain_text: string;
-        href: any;
-      }[];
-    };
-    published: {
-      id: string;
-      type: string;
-      checkbox: boolean;
-    };
-    author: {
-      id: string;
-      type: string;
-      rich_text: {
-        type: string;
-        text: {
-          content: string;
-          link: any;
-        };
-        annotations: {
-          bold: boolean;
-          italic: boolean;
-          strikethrough: boolean;
-          underline: boolean;
-          code: boolean;
-          color: string;
-        };
-        plain_text: string;
-        href: any;
-      }[];
-    };
-    tags: {
-      id: string;
-      type: string;
-      multi_select: {
-        id: string;
-        name: string;
-        color: string;
-      }[];
-    };
-    postedAt: {
-      id: string;
-      type: string;
-      date: {
-        start: string;
-        end: any;
-        time_zone: any;
-      };
-    };
-    title: {
-      id: string;
-      type: string;
-      title: {
-        type: string;
-        text: {
-          content: string;
-          link: any;
-        };
-        annotations: {
-          bold: boolean;
-          italic: boolean;
-          strikethrough: boolean;
-          underline: boolean;
-          code: boolean;
-          color: string;
-        };
-        plain_text: string;
-        href: any;
-      }[];
-    };
-  };
-  url: string;
-};
-
+export const blog = new BlogClient({
+  client,
+  database_id: getEnv().NOTION_DATABASE_ID
+});
