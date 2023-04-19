cbeaa96befd9b1902f32d20964e576d04ab31a75
IshanKBG <jaiswalishan701@gmail.com>
2023-04-19 16:20:30 +0530

    Inital commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,27 +1,22 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
+import { type KVNamespace } from '@cloudflare/workers-types';
+export const useServerTimeLoader = routeLoader$(async ({ platform }) => {
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-export const useServerTimeLoader = routeLoader$(() => {
+  const { app } = platform as { app: KVNamespace }
   return {
+    message: await app.get('message'),
     date: new Date().toISOString(),
   };
 });
 
 export default component$(() => {
+
+  const data = useServerTimeLoader()
   return (
-    <div class="page">
-      <main>
-        <Header />
-        <Slot />
-      </main>
-      <div class="section dark">
-        <div class="container">
-          <Footer />
-        </div>
-      </div>
-    </div>
+    <main>
+      {data.value.message}
+      <Slot />
+    </main>
   );
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
+import { Miniflare, LogLevel, Log } from "miniflare"
 import tsconfigPaths from 'vite-tsconfig-paths';
+async function getMiniflareBindings() {
+  const mf = new Miniflare({
+    log: new Log(LogLevel.INFO),
+    kvPersist: './kv-data', // Use filebase or in memory store
+    kvNamespaces: ['app'], //Declare array with NameSpaces
+    globalAsyncIO: true,
+    globalTimers: true,
+    globalRandom: true,
 
-export default defineConfig(() => {
+    script: `
+		addEventListener("fetch", (event) => {
+			event.waitUntil(Promise.resolve(event.request.url));
+			event.respondWith(new Response(event.request.headers.get("X-Message")));
+		});
+		addEventListener("scheduled", (event) => {
+			event.waitUntil(Promise.resolve(event.scheduledTime));
+		});
+		`
+  });
+  return mf.getBindings()
+}
+
+export default defineConfig(async () => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity({
+      platform: await getMiniflareBindings(),
+    }), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
